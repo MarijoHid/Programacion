1618827da011a0ca82e29f5b3b1d47b4739d7b8c
Majo <mariajhidal@gmail.com>
2022-11-07 20:46:25 -0300

    Subo Cambios con javaS

---
+++ index.js
@@ -0,0 +1,27 @@
+let nombre = 'Maria'
+
+console.log(typeof nombre)
+console.log(nombre)
+
+let apellido= 'Hidalgo'
+
+console.log(typeof apellido)
+console.log(apellido)
+
+let edad= 26;
+console.log(typeof edad)
+console.log(edad)
+
+let mascota= "Cobayo";
+console.log(typeof mascota)
+console.log(mascota)
+
+let entryNumber = prompt ("Ingrese un valor númerico")
+
+if (entryNumber > 5) {
+
+    console.log("El número es mayor a 5");
+    
+    } else {
+    console.log("El número es menor o igual 5");
+    }
